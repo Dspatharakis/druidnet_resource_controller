f4f66ddb57c24d0bddf43159c1616b9fdeeb722a
Dspatharakis <dhmhtrhs.spath@gmail.com>
2022-03-11 17:43:50 +0200

    implemet keda hpa based on aimd algorithm

--- project/tasks.py
+++ project/tasks.py
@@ -9,45 +9,45 @@ from celery.utils.log import get_task_logger
 from PIL import Image
 import os
 import math
+from functools import reduce
 
 logger = get_task_logger(__name__)
 
 @celery.task(name="create_task_red", queue="red")
 def create_task_red(img_id,start_time): # kubernetes access with nodeport and exposed port
-    preprocess_time = time.time()-start_time
-    print ("preprocess_time:" , preprocess_time)
+    with db.session() as conn:
+        conn.execute("UPDATE rates SET active_requests_app1 = active_requests_app1 + 1")
+        conn.commit()
     try:
         result = requests.post("http://app1:6004/",files={"file": open('/tmp/'+img_id,"rb")}) #"http://app1:6004/", timeout=45)
     except requests.exceptions.ConnectionError as err:
         print('Error while trying to post once: {}'.format(err))
-    rt = time.time() - start_time
-    print ("rt:" , rt)
     os.remove('/tmp/'+img_id)
-    data = Rate.query.first()
     art = time.time() - start_time
-    data.accumulative_response_time = data.accumulative_response_time + art
-    data.active_requests_app1 = data.active_requests_app1 - 1
-    data.counter_requests = data.counter_requests + 1
-    db.session.commit()
+    with db.session() as conn:
+        conn.execute("UPDATE rates SET active_requests_app1 = active_requests_app1 - 1")
+        conn.execute("UPDATE rates SET accumulative_response_time = accumulative_response_time + '%s'" % art)
+        conn.execute("UPDATE rates SET counter_requests = counter_requests + 1")
+        conn.commit()
     return True
 
 @celery.task(name="create_task_green", queue="green")
 def create_task_green(img_id, start_time):
-    preprocess_time = time.time()-start_time
-    print ("preprocess_time:" , preprocess_time)
+    with db.session() as conn:
+        conn.execute("UPDATE rates SET active_requests_app2 = active_requests_app2 + 1")
+        conn.commit()
     try:
         result = requests.post("http://app2:6005/",files={"file": open('/tmp/'+img_id,"rb")}) #"http://app2:6005/", timeout=45)
     except requests.exceptions.ConnectionError as err:
         print('Error while trying to post once: {}'.format(err))
-    rt = time.time() - start_time
-    print ("rt:" , rt)
     os.remove('/tmp/'+img_id)
-    data = Rate.query.first()
     art = time.time() - start_time
-    data.accumulative_response_time = data.accumulative_response_time + art
-    data.active_requests_app2 = data.active_requests_app2 - 1
-    data.counter_requests = data.counter_requests + 1
-    db.session.commit()
+    with db.session() as conn:
+        conn.execute("UPDATE rates SET active_requests_app2 = active_requests_app2 - 1")
+        conn.execute("UPDATE rates SET accumulative_response_time = accumulative_response_time + '%s'" % art)
+        conn.execute("UPDATE rates SET counter_requests = counter_requests + 1")
+        conn.commit()
+    
     return True
 
 @celery.task(name="create_task_queue", queue="queue")
@@ -56,15 +56,21 @@ def create_task_queue(img_id,start_time):
     data = Rate.query.first()
     req_rate_app1 = data.req_rate_app1 
     req_rate_app2 = data.req_rate_app2 
-    propability = random.randint(0,(int(req_rate_app1)+int(req_rate_app2)))
-    data = Rate.query.first()
-    if propability < req_rate_app1:  
-        task = create_task_red.delay(img_id,start_time)
-        data.active_requests_app1 = data.active_requests_app1 + 1
+    # for i in range(2):  #TODO scale for workers 
+        # sum += placement[topology][i] * workload_per_vdu[i] 
+        # d[i] = placement[topology][i] * workload_per_vdu[i] 
+    sum = req_rate_app1 + req_rate_app2
+    d = [req_rate_app1, req_rate_app2]
+    p = map(lambda pc:pc/sum,d)
+    c = reduce(lambda c, x: c+[c[-1]+x], p,[0])[1:]
+    rand = random.uniform(0,1)
+    host_id = next(i for i, v in enumerate(c) if v > rand)
+    print ("Host id:", host_id)
+    # host_id=1
+    if host_id == 0:  
+        create_task_red.delay(img_id,start_time)
     else: 
-        task = create_task_green.delay(img_id,start_time)
-        data.active_requests_app2 = data.active_requests_app2 + 1
-    db.session.commit()
+        create_task_green.delay(img_id,start_time)
     return True
 
 @celery.task(queue='celery_periodic')
@@ -75,15 +81,25 @@ def update_per_interval():
     try:
         data = Rate.query.first()
         time_passed = 1
-        if data.time_passed_since_last_event >= int(os.environ.get("event_cooldown", "2")): 
+        if data.time_passed_since_last_event >= int(os.environ.get("event_cooldown", "0")): 
             if (length == 0):
-                print ("queue is empty")
-                data.process_rate_app1 = float(os.environ.get("beta1", "0.5")) *  data.req_rate_app1 + math.sqrt(2* float(os.environ.get("alpha1", "2"))*data.active_requests_app1)
-                data.process_rate_app2 = float(os.environ.get("beta2", "0.5")) *  data.req_rate_app2 + math.sqrt(2* float(os.environ.get("alpha2", "2"))*data.active_requests_app2)
+                print ("Event! queue is empty")
+                try: 
+                    data.process_rate_app1 = float(os.environ.get("beta1", "0.5")) *  data.req_rate_app1 + math.sqrt(2* float(os.environ.get("alpha1", "2"))*data.active_requests_app1)
+                except ValueError as err:
+                    print('Error while trying to calculate process rate1: {}'.format(err))
+                    data.process_rate_app1 = float(os.environ.get("beta1", "0.5")) *  data.req_rate_app1
+                try:
+                    data.process_rate_app2 = float(os.environ.get("beta2", "0.5")) *  data.req_rate_app2 + math.sqrt(2* float(os.environ.get("alpha2", "2"))*data.active_requests_app2)
+                except ValueError as err:
+                    print('Error while trying to calculate process rate2: {}'.format(err))
+                    data.process_rate_app2 = float(os.environ.get("beta2", "0.5")) *  data.req_rate_app2
                 # Need a proper mapping between \gamma and replicas
-                data.replicas_app1 = data.process_rate_app1 // data.process_rate_app1
-                data.replicas_app2 = data.process_rate_app2 // data.process_rate_app2
-                data.queue_trigger = 2 
+                data.replicas_app1 = data.process_rate_app1 // float(os.environ.get("flavor_app1", "2"))
+                if data.replicas_app1 < 1 : data.replicas_app1 = 1
+                data.replicas_app2 = data.process_rate_app2 // float(os.environ.get("flavor_app2", "10"))
+                if data.replicas_app2 < 1 : data.replicas_app2 = 1
+                data.queue_trigger = 2  #TODO: remove
                 data.time_passed_since_last_event = 0 
                 time_passed = 0
             else:
@@ -98,7 +114,6 @@ def update_per_interval():
         data.time_of_experiment = data.time_of_experiment + 1
         data.queue_size = length
         data.interval_time = data.interval_time + 1
-
         if data.counter_requests > 0:
             art = data.accumulative_response_time / data.counter_requests
             data.average_response_time = art
